d92a113e83c9e43614d07148c102a7d8b8f5d48b
Harishan Ravindranathan <harishanravindranathan@Harishans-MacBook-Pro-2.local>
2023-05-06 11:15:49 -0400

    changed redirect uri

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ dotenv.config()
 var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
 var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
 
-var spotify_redirect_uri = 'http://localhost:3000/auth/callback'
+//var spotify_redirect_uri = 'http://localhost:3000/auth/callback'
+var spotify_redirect_uri = 'https://harishan-r.github.io/auth/callback'
 
 var generateRandomString = function (length) {
   var text = '';
